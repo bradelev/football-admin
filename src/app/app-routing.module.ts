3a5b29fcfc0eb362712cf2f792d1cefe75db9d91
bradelev <bradelev85@gmail.com>
2021-01-13 20:30:31 -0300

    Add modules and routing for Club and Players (#12)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'players',
+    loadChildren: () => import('./players/players.module').then(
+      mod => mod.PlayersModule
+    )
+  },
+  {
+    path: 'clubs',
+    loadChildren: () => import('./clubs/clubs.module').then(
+      mod => mod.ClubsModule
+    )
+  },
+  {
+    path: '**',
+    redirectTo: 'players'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
